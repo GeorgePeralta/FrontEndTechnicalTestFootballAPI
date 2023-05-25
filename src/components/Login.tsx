5f482cb489472ec8fb990f3bb836092392e7684f
George <lucasperalta1993@hotmail.com>
2023-05-25 10:21:56 -0300

    refactor: removing logic and leaving only the page name

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,28 +1,5 @@
-import React, { useState } from 'react';
-
-const Login: React.FC = () => {
-  const [apiKey, setApiKey] = useState('');
-
-  const handleApiKeyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setApiKey(event.target.value);
-  };
-
-  const handleLogin = () => {    
-    console.log(apiKey);
-  };
-
+export default function Login() {
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Enter API Key"
-        value={apiKey}
-        onChange={handleApiKeyChange}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
-  );
-};
-
-export default Login;
+    <div>Login</div>
+  )
+}
